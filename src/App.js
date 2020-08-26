36ecade7ca66fa928301b478d47de438bc260998
Cameron Moore <cmrnmr@gmail.com>
2020-08-26 17:38:08 -0400

    created logic for grid structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const rowPlaces = 50;
+const colValues = 50;
+
+const App = () => {
+  // gridIs --> rows
+  const [gridIs, setGrid] = useState(() => {
+    // initialize outer array
+    const rows = [];
+    // for as many number of 'rowplaces'...(50)
+    for (let i = 0; i < rowPlaces; i++) {
+      // produce an array w/ length of 'colValues' populated by 0's... (50)
+      rows.push(Array.from(Array(colValues), () => 0));
+    }
+    // rows --> array of 50 arrays, each containing 50 0's
+    return rows;
+  });
+
+  console.log(gridIs);
+  return <div className="App"></div>;
+};
 
 export default App;
