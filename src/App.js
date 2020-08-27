6cb4a3d265f92e1eedfc9e60895f0692ab388262
Cameron Moore <cmrnmr@gmail.com>
2020-08-27 19:06:44 -0400

    implemented is growing state and usecallback to preserve run function from re-rendering after state change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useRef } from "react";
 import styled from "styled-components";
 import produce from "immer";
 // import "./App.css";
@@ -25,33 +25,64 @@ const App = () => {
     return rows;
   });
 
+  const [growing, setGrowing] = useState(false);
+
+  const growingRef = useRef(growing);
+  growingRef.current = growing;
+
+  //trigger function should be immune to re-rendering
+  //trigger function only gets created once in time and does not re-render - it does not recreate itself with a new version of growing after its state is updated
+  //but the state it wants to access will be changing
+  //to provide the persisting function with access to the changing state we need to 'hold' the state somewhere outside of its hook with a useref
+  const catalyze = useCallback(() => {
+    //if not growing, return outside of the scope of immunity that useCallback provides to preserve against re-rendering
+    //basecase
+    if (!growingRef) {
+      return;
+    }
+
+    //trigger growth
+    setTimeout(catalyze, 1000);
+  }, []);
+
   return (
-    <Box>
-      {gridIs.map((rows, i) =>
-        rows.map((col, j) => (
-          <div
-            onClick={() => {
-              //                  current state, updated state
-              const newGrid = produce(gridIs, (gridCopy) => {
-                //updated cell position boolean toggle
-                gridCopy[i][j] = gridIs[i][j] ? 0 : 1;
-              });
-              //setState to updated state containing updated cell positions
-              setGrid(newGrid);
-            }}
-            key={`${i}-${j}`}
-            style={{
-              height: 18,
-              width: 18,
-              margin: "1px 1px auto",
-              borderRadius: "100%",
-              //color is green if cell position is toggled true otherwise its undefined (blank)
-              backgroundColor: gridIs[i][j] ? "#9DE0AD" : undefined,
-            }}
-          />
-        ))
-      )}
-    </Box>
+    <>
+      <button
+        onClick={() => {
+          setGrowing(!growing);
+        }}
+      >
+        Start Growth
+      </button>
+      <Box>
+        {/* a cell coordinate is determined by the axes of i and j */}
+        {gridIs.map((rows, i) =>
+          rows.map((col, j) => (
+            <div
+              onClick={() => {
+                //                  current state, updated state
+                const newGrid = produce(gridIs, (gridCopy) => {
+                  //updated cell position boolean toggle
+                  gridCopy[i][j] = gridIs[i][j] ? 0 : 1;
+                });
+                //setState to updated state containing updated cell positions
+                setGrid(newGrid);
+              }}
+              key={`${i}-${j}`}
+              style={{
+                height: 18,
+                width: 18,
+                margin: "1px 1px auto",
+                borderRadius: "80% 0% 80%",
+                border: "solid 1px #a6cc7099",
+                //color is green if cell position is toggled true otherwise its undefined (blank)
+                backgroundColor: gridIs[i][j] ? "#9DE0AD" : undefined,
+              }}
+            />
+          ))
+        )}
+      </Box>
+    </>
   );
 };
 
